2ee6ce36464699f4b1f1158c7ee36c134acdfa14
Jack Rowe <jack.rowe@outlook.com>
2024-04-15 23:05:59 +0100

    more mesh shit

--- mesh.cpp
+++ mesh.cpp
@@ -16,5 +16,67 @@ void mesh::clean() {
 }
 
 bool mesh::load(const std::string& path) {
+	clean();
+
+	// open the file from the provided path
+	std::ifstream file;
+	file.open(path);
+
+	// make sure the file is valid
+	if (!file || !file.is_open() || !file.good()) return false;
+
+	std::string line = "";
+
+	// loop over every line of the file
+	while (getline(file, line))
+	{
+		// get the header of the current line and remove it from the line afterwards
+		objHeader currentHeader = stringToHeader[line.substr(0, line.find_first_of(' '))];
+		line = line.substr(line.find_first_of(' ') + 1, line.length());
+
+		switch (currentHeader) {
+			case vertex: {
+				Vector3<float> v = { 0.0f };
+
+				// loop over each component of the vertex and add it to v before removing that component from the line
+				for (int i = 0; i < 3; i++)
+				{
+					std::string s = line.substr(0, line.find_first_of(' '));
+
+					if (i == 0)
+					{
+						v.x = std::stof(s);
+					}
+					else if (i == 1)
+					{
+						v.y = std::stof(s);
+					}
+					else
+					{
+						v.z = std::stof(s);
+					}
+
+					line = line.substr(line.find_first_of(' ') + 1, line.length());
+				}
+			}
+
+			case uv: {
+				break;
+			}
+
+			case normal: {
+				break;
+			}
+
+			case face: {
+				break;
+			}
+
+			default: {
+				break;
+			}
+		}
+	}
+
 	return true;
 }
--- mesh.h
+++ mesh.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <string>
+#include <fstream>
+#include <map>
 
 #include "vectors.h"
 
@@ -10,9 +12,33 @@ class mesh
 private:
 	std::vector<Vector3<float>*> vertices = { {} };
 	std::vector<Vector3<float>*> normals = { {} };
-	std::vector<Vector3<float>*> uvs = { {} };
-	std::vector<Vector3<int>*> faces = { {} };
+	std::vector<Vector2<float>*> uvs = { {} };
+	std::vector<std::vector<Vector3<int>>> faces = { { {} } };
 public:
+	enum objHeader {
+		none,
+		vertex,
+		normal,
+		uv,
+		face,
+		smoothing,
+		object,
+		material,
+		comment
+	};
+
+	std::map<std::string, objHeader> stringToHeader = {
+		{"mtllib", material},
+		{"usemtl", material},
+		{"o", object},
+		{"v", vertex},
+		{"vn", normal},
+		{"vt", uv},
+		{"s", smoothing},
+		{"f", face},
+		{"#", comment},
+	};
+
 	mesh(const std::string& path = "");
 	~mesh();
 
