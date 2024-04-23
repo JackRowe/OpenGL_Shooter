61bad624134d394d6bdebe1cd7d693e64fa8b00d
Jack Rowe <jack.rowe@outlook.com>
2024-04-23 09:58:06 +0100

    YIEPEEEEEEEEE

--- input.cpp
+++ input.cpp
@@ -12,7 +12,7 @@ void input::updateInputVector() {
 	int forward = keyStates['s'] - keyStates['w'];
 	int horizontal = keyStates['d'] - keyStates['a'];
 	int vertical = keyStates['e'] - keyStates['q'];
-
+	//std::cout << horizontal << ", " << vertical << ", " << forward << std::endl;
 	inputVector = { horizontal, vertical, forward };
 }
 
@@ -23,42 +23,6 @@ void input::setMouseState(int button, int state, int x, int y) {
 }
 
 void input::updateMouseMotion(int x, int y) {
-
-	// reset flag
-	
-	//if (x == WINDOW_WIDTH / 2 && y == WINDOW_HEIGHT / 2) {
-	//	// at centre of the screen
-	//	std::cout << "ignore: " << x - mousePosition.x << ", " << y - mousePosition.y << " | new: (" << x << ", " << y << ") | old: (" << mousePosition.x << ", " << mousePosition.y << ")" << std::endl;
-	//	mouseDelta = { 0 };
-	//}
-	//else if ((x - mousePosition.x) + x == WINDOW_WIDTH / 2 && (y - mousePosition.y) + y == WINDOW_HEIGHT / 2) {
-	//	// wants to go to centre of the screen
-	//	std::cout << "ignore: " << x - mousePosition.x << ", " << y - mousePosition.y << " | new: (" << x << ", " << y << ") | old: (" << mousePosition.x << ", " << mousePosition.y << ") | ???: (" << (x - mousePosition.x) + x << ", " << (y - mousePosition.y) + y << ")" << std::endl;
-	//	mouseDelta = { 0 };
-	//}
-	//else {
-	//	// apply
-	//	std::cout << "apply: " << x - mousePosition.x << ", " << y - mousePosition.y << " | new: (" << x << ", " << y << ") | old: (" << mousePosition.x << ", " << mousePosition.y << ")" << std::endl;
-	////	mouseDelta = { x - mousePosition.x, y - mousePosition.y };
-	//}
-
-	//if (!ignoreMouseFlag) {
-	//	mouseDelta = { 0 };
-	//}
-	//else {
-	//	mouseDelta = { x - mousePosition.x, y - mousePosition.y };
-	//}
-
-	//if (mousedelta.x > 10 || mousedelta.y > 10)
-	//{
-	//	mousedelta.x = 0;
-	//	mousedelta.y = 0;
-	//}
-
 	mouseDelta = { x - mousePosition.x, y - mousePosition.y };
 	mousePosition = { x, y };
-
-	/*if (abs(mousePosition.x - WINDOW_WIDTH) >= WINDOW_WIDTH / 2 || abs(mousePosition.y - WINDOW_HEIGHT) >= WINDOW_HEIGHT / 2) {
-		glutWarpPointer(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
-	}*/
 }
